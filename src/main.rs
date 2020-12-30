b7b72c1f793e8867b924bf05e55e7b3338c82fbd
Mat Trudel <mat@geeky.net>
2020-12-30 15:59:45 -0500

    Basic HTTP service

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-fn main() {
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
     let mut builder = env_logger::builder();
     builder.parse_filters("libmdns=info");
+    builder.parse_filters("hap=info");
     builder.init();
 
     let responder = libmdns::Responder::new().unwrap();
@@ -11,7 +15,10 @@ fn main() {
         &["c#=2", "ff=0", "id=11:22:33:44:55:77", "md=hapco", "pv=1.1", "s#=1", "sf=1", "ci=2", "sh=T15I+A=="],
     );
 
-    loop {
-        ::std::thread::sleep(::std::time::Duration::from_secs(10));
-    }
+    let log = warp::log("hap");
+    let routes = warp::any().map(|| "Hello, World!").with(log);
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], 5000))
+        .await;
 }
